8bd22b8bcaedc76c22b30cfabf9ccd300876df88
THEBIGMAN777 <tlockonon777@gmail.com>
2023-07-05 14:44:49 +0530

    react app

---
+++ react/myapp/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from 'C:/Users/chara/OneDrive/Desktop/GLOBAL COURSE/react/myapp/src/components/Header.js';
+function App() {
+  return (
+    <div className="App">
+      <Header/> 
+    </div>
+  );
+}
+
+export default App;
