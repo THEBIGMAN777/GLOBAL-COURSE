22e48afcae83e98442820ce0764ab31df4572dcc
THEBIGMAN777 <tlockonon777@gmail.com>
2023-07-07 12:12:24 +0530

    Today

--- Flask Tutorial/App.py
+++ Flask Tutorial/App.py
@@ -1,11 +1,14 @@
 from flask import Flask
-print("__name__",__name__)
+print("__name__", __name__)
 app = Flask(__name__)
 @app.route("/")
 def welcome():
     return 'Welcome to Flask '
-@app.route("/greet")
-def greet():
-    return "Greetings from Flask"
+@app.route("/greetinperson/<string:name>")
+def personalizedgreeting(name):
+    return f"{name} Greetings from Flask"
+@app.route("/add/<int:num1>/<int:num2>")
+def add(num1,num2):
+    return f"The result of addition is {str(num1+num2)}"
 if(__name__ == "__main__"):
-    app.run()
+    app.run(debug=True)
