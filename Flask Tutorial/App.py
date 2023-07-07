e0b2577aaa894992d0a48bb035d80374adf63555
THEBIGMAN777 <tlockonon777@gmail.com>
2023-07-07 12:36:08 +0530

    Today

--- Flask Tutorial/App.py
+++ Flask Tutorial/App.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 print("__name__", __name__)
 app = Flask(__name__)
 @app.route("/")
@@ -10,5 +10,11 @@ def personalizedgreeting(name):
 @app.route("/add/<int:num1>/<int:num2>")
 def add(num1,num2):
     return f"The result of addition is {str(num1+num2)}"
+@app.route("/users/add",methods=["post"])
+def addUser():
+    user = request.get_json()
+    print("Post Method")
+    return user
+
 if(__name__ == "__main__"):
     app.run(debug=True)
