7a1db9c3eef39327371bc6a8fb53114c1e45526e
THEBIGMAN777 <tlockonon777@gmail.com>
2023-07-07 10:04:58 +0530

    c

--- react/myapp/src/App.js
+++ react/myapp/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from 'C:/Users/chara/OneDrive/Desktop/GLOBAL COURSE/react/myapp/src/components/Header.js';
 import Content from 'C:/Users/chara/OneDrive/Desktop/GLOBAL COURSE/react/myapp/src/components/Content.js';
