29f6a792dcc301a900ec3c1e3996063d6554f02d
THEBIGMAN777 <tlockonon777@gmail.com>
2023-07-10 11:28:04 +0530

    today

--- CONTEXTDEMO/context1/src/App.js
+++ CONTEXTDEMO/context1/src/App.js
@@ -1,25 +1,79 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, createContext, useContext } from "react";
+import Employee from './COMPONENTS/Employee';
 
-function App() {
+const UserContext = createContext();
+
+/*function App() {
+  const [user, setUser] = useState("Jesse Hall");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 info={user}></Component2>
+      </UserContext.Provider>
     </div>
   );
 }
+
+function Component2(props) {
+ let name = props.info
+  return (
+    <>
+      <h1>{name}</h1>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
+*/
+function App() {
+  const [employees,setEmployees]=useState([])
+
+  const saveEmployeeHandler=(code,name,dept,sal)=>{
+      let emp = {code:code,name:name,dept:dept,sal:sal}
+      console.log("emp #",emp)
+      setEmployees([...employees,emp])
+      console.log('employees ##',employees)    
+  
+    }
+
+  return (
+      <>
+        <Employee saveEmployee={saveEmployeeHandler} empdata={employees}/>
+      </>
+    );
+}
+
 
 export default App;
